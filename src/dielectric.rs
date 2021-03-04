724c346f8cd1544d07858d49354bd76a1fd9e3b9
Justin Liew <jliew@fastly.com>
2021-03-04 12:58:04 -0800

    internal reflection

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -2,7 +2,7 @@ use crate::vec3::{Colour,Vec3};
 use crate::material::Material;
 use crate::ray::Ray;
 use crate::hittable::HitRecord;
-use crate::utils::{refract};
+use crate::utils::{reflect, refract};
 
 pub struct Dielectric {
 	ir: f64,
@@ -23,11 +23,17 @@ impl Material for Dielectric {
 			false => self.ir
 		};
 		let unit_direction = Vec3::unit_vector(r_in.dir);
-		let refracted = refract(&unit_direction, &rec.normal, refraction_ratio);
+		let cos_theta = f64::min(Vec3::dot(&-unit_direction, &rec.normal), 1.0);
+		let sin_theta = f64::sqrt(1.0 - cos_theta*cos_theta);
+
+		let direction = match refraction_ratio * sin_theta > 1.0 {
+			true => reflect(&unit_direction, &rec.normal),
+			false => refract(&unit_direction, &rec.normal, refraction_ratio),
+		};
 
 		Some(
 			(
-				Ray::new(&rec.p, &refracted),
+				Ray::new(&rec.p, &direction),
 				Colour::new(1.0,1.0,1.0),
 			)
 		)
