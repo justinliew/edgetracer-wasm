f22010cb0eea2c8f0c6008c968955df0491cff7f
Justin Liew <jliew@fastly.com>
2021-03-13 08:22:43 -0800

    fix integration

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ use fastly::{mime, Error, Request, Response};
 mod vec3;
 mod ray;
 mod camera;
-mod dielectric;
+mod hitrecord;
 mod hittable;
-mod sphere;
 mod hittable_list;
 mod material;
-mod lambertian;
-mod metal;
 mod utils;
 mod render;
 
